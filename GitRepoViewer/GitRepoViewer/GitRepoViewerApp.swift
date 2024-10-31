89570bf16a1a31567954aedbbdd1b3204696f4d6
Rafael Afonso <rafaelafonso1980@gmail.com>
2024-10-31 07:15:55 +0000

    First commit

---
+++ GitRepoViewer/GitRepoViewer/GitRepoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRepoViewerApp.swift
+//  GitRepoViewer
+//
+//  Created by Rafael Afonso on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitRepoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
